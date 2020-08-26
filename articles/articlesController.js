4153b71aab1707c6be93c891323586f9040ee1f3
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-08-26 10:46:45 -0300

    article delete, articles list

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -5,7 +5,15 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-  res.send("ROTAS DE ARTIGOS");
+  Article.findAll({
+    include: [
+      {
+        model: Category,
+      },
+    ],
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
@@ -25,8 +33,28 @@ router.post("/articles/save", (req, res) => {
     body: body,
     categoryId: category,
   }).then(() => {
-    res.redirect('/admin/articles')
+    res.redirect("/admin/articles");
   });
 });
+
+router.post("/articles/delete", (req, res) => {
+  let id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      res.redirect("/admin/articles");
+    }
+  } else {
+    res.redirect("/admin/articles");
+  }
+});
+
 
 module.exports = router;
